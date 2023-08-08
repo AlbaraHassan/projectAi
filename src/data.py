3635ef9d84b497b37bcb8c3e2c389f2a584a6cfa
Ajdin Jajcanin <jajcanin.ajdin@gmail.com>
2023-08-08 21:10:26 +0200

    feat: updated components

---
+++ src/data.py
@@ -0,0 +1,192 @@
+import math
+import torch
+import torch.utils.data
+from pathlib import Path
+from torchvision import datasets, transforms
+import multiprocessing
+
+from .helpers import compute_mean_and_std, get_data_location
+import matplotlib.pyplot as plt
+
+
+def get_data_loaders(
+        batch_size: int = 32, valid_size: float = 0.2, num_workers: int = -1, limit: int = -1
+):
+    if num_workers == -1:
+        # Use all cores
+        num_workers = multiprocessing.cpu_count()
+
+    data_loaders = {"train": None, "valid": None, "test": None}
+
+    base_path = Path(get_data_location())
+
+    mean, std = compute_mean_and_std()
+    print(f"Dataset mean: {mean}, std: {std}")
+
+    data_transforms = {
+        "train": transforms.Compose([
+            transforms.Resize(256),
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(30),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+            transforms.RandomAffine(degrees=0, translate=(0.1, 0.1), scale=(0.8, 1.2)),
+            transforms.RandomApply([transforms.GaussianBlur(kernel_size=23)], p=0.2),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=mean, std=std),
+        ]),
+        "valid": transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=mean, std=std),
+        ]),
+        "test": transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=mean, std=std),
+        ]),
+    }
+    train_data = datasets.ImageFolder(
+        base_path / "train",
+        transform=data_transforms["train"]
+    )
+    valid_data = datasets.ImageFolder(
+        base_path / "train",
+        transform=data_transforms["valid"]
+    )
+
+    n_tot = len(train_data)
+    indices = torch.randperm(n_tot)
+
+    if limit > 0:
+        indices = indices[:limit]
+        n_tot = limit
+
+    split = int(math.ceil(valid_size * n_tot))
+    train_idx, valid_idx = indices[split:], indices[:split]
+
+    train_sampler = torch.utils.data.SubsetRandomSampler(train_idx)
+    valid_sampler = torch.utils.data.SubsetRandomSampler(valid_idx)
+
+    data_loaders["train"] = torch.utils.data.DataLoader(
+        train_data,
+        batch_size=batch_size,
+        sampler=train_sampler,
+        num_workers=num_workers,
+    )
+    data_loaders["valid"] = torch.utils.data.DataLoader(
+        valid_data,
+        batch_size=batch_size,
+        sampler=valid_sampler,
+        num_workers=num_workers,
+    )
+
+    test_data = datasets.ImageFolder(
+        base_path / "test",
+        transform=data_transforms["test"]
+    )
+
+    if limit > 0:
+        indices = torch.arange(limit)
+        test_sampler = torch.utils.data.SubsetRandomSampler(indices)
+    else:
+        test_sampler = None
+
+    data_loaders["test"] = torch.utils.data.DataLoader(
+        test_data,
+        batch_size=batch_size,
+        sampler=test_sampler,
+        num_workers=num_workers,
+        shuffle=False if test_sampler else True,
+    )
+
+    return data_loaders
+
+
+def visualize_one_batch(data_loaders, max_n: int = 5):
+    """
+    Visualize one batch of data.
+
+    :param data_loaders: dictionary containing data loaders
+    :param max_n: maximum number of images to show
+    :return: None
+    """
+
+    # YOUR CODE HERE:
+    # obtain one batch of training images
+    # First obtain an iterator from the train dataloader
+    dataiter = iter(data_loaders["train"])
+
+    # Iterate through the DataLoader to obtain a batch of images and labels
+    images, labels = next(dataiter)
+
+    # Undo the normalization (for visualization purposes)
+    mean, std = compute_mean_and_std()
+    invTrans = transforms.Compose(
+        [
+            transforms.Normalize(mean=[0.0, 0.0, 0.0], std=1 / std),
+            transforms.Normalize(mean=-mean, std=[1.0, 1.0, 1.0]),
+        ]
+    )
+
+    images = invTrans(images)
+
+    # YOUR CODE HERE:
+    # Get class names from the train data loader
+    class_names = data_loaders["train"].dataset.classes
+
+    # Convert from BGR (the format used by pytorch) to
+    # RGB (the format expected by matplotlib)
+    images = torch.permute(images, (0, 2, 3, 1)).clip(0, 1)
+
+    # plot the images in the batch, along with the corresponding labels
+    fig = plt.figure(figsize=(25, 4))
+    for idx in range(max_n):
+        ax = fig.add_subplot(1, max_n, idx + 1, xticks=[], yticks=[])
+        ax.imshow(images[idx])
+        # print out the correct label for each image
+        # .item() gets the value contained in a Tensor
+        ax.set_title(class_names[labels[idx].item()])
+
+
+######################################################################################
+#                                     TESTS
+######################################################################################
+import pytest
+
+
+@pytest.fixture(scope="session")
+def data_loaders():
+    return get_data_loaders(batch_size=2, num_workers=0)
+
+
+def test_data_loaders_keys(data_loaders):
+    assert set(data_loaders.keys()) == {"train", "valid",
+                                        "test"}, "The keys of the data_loaders dictionary should be train, valid and test"
+
+
+def test_data_loaders_output_type(data_loaders):
+    # Test the data loaders
+    dataiter = iter(data_loaders["train"])
+    images, labels = next(dataiter)
+
+    assert isinstance(images, torch.Tensor), "images should be a Tensor"
+    assert isinstance(labels, torch.Tensor), "labels should be a Tensor"
+    assert images[0].shape[-1] == 224, "The tensors returned by your dataloaders should be 224x224. Did you " \
+                                       "forget to resize and/or crop?"
+
+
+def test_data_loaders_output_shape(data_loaders):
+    dataiter = iter(data_loaders["train"])
+    images, labels = next(dataiter)
+
+    assert len(images) == 2, f"Expected a batch of size 2, got size {len(images)}"
+    assert (
+            len(labels) == 2
+    ), f"Expected a labels tensor of size 2, got size {len(labels)}"
+
+
+def test_visualize_one_batch(data_loaders):
+    visualize_one_batch(data_loaders, max_n=2)
---
+++ src/model.py
@@ -0,0 +1,72 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+class MyModel(nn.Module):
+    def __init__(self, num_classes: int = 1000, dropout: float = 0.7) -> None:
+        super(MyModel, self).__init__()
+
+        # Define a CNN architecture with two convolutional layers
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 14 * 14, 1024),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+
+            nn.Linear(512, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+import pytest
+
+
+@pytest.fixture(scope="session")
+def data_loaders():
+    from .data import get_data_loaders
+
+    return get_data_loaders(batch_size=2)
+
+
+def test_model_construction(data_loaders):
+    model = MyModel(num_classes=23, dropout=0.3)
+
+    dataiter = iter(data_loaders["train"])
+    images, labels = next(dataiter)
+
+    out = model(images)
+
+    assert isinstance(
+        out, torch.Tensor
+    ), "The output of the .forward method should be a Tensor of size ([batch_size], [n_classes])"
+
+    assert out.shape == torch.Size(
+        [2, 23]
+    ), f"Expected an output tensor of size (2, 23), got {out.shape}"
